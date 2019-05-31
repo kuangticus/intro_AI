2483a4c927ec12093f8e1f13b7646e303af73311
WeiHao Kuang <kuangw@flip2.engr.oregonstate.edu>
2019-05-31 15:27:26 -0700

    featurizing

--- sentiment.cpp
+++ sentiment.cpp
@@ -5,28 +5,34 @@
 #include <algorithm>
 #include <boost/algorithm/string.hpp>
 #include <unordered_map>
+#include <cmath>
+#include <climits>
+#include <sstream>
 
 using namespace std;
 
+int positiveTot = 0, negativeTot = 0;
+
 void parse (vector<string> *, int);
-void presentOut (vector<int> *, vector<string>, int);
+void featurize (vector<vector<int>> *, vector<string>, int);
+void printer (vector<vector<int>> *, vector<string>, int);
 
 // add the bayesian stuff here
 
-int main (int argc, char**argv){ // maybe add a little of user input for file choosing
+int main (int argc, char** argv){ // maybe add a little of user input for file choosing
 
     // vectors for the test and training bank
     vector<string> wordbankTraining, wordbankTesting;
-    vector<int> featuresTraining, featuresTesting;
+    vector<vector<int>> featuresTraining, featuresTesting;
     
     parse( &wordbankTraining, 0);
-    parse( &wordbankTesting, 1);
+    // parse( &wordbankTesting, 1);
+    featurize ( &featuresTraining, wordbankTraining, 0);
 
-    for ( int i = 0; i < wordbankTraining.size(); i++){
-        cout << wordbankTraining[i] << endl;
-    }
-
-    return 0;
+    // for ( int i = 0; i < wordbankTraining.size(); i++){
+    //     cout << wordbankTraining[i] << endl;
+    // }
+    // return 0;
 }
 
 void parse(vector<string> *wordbank, int setting){
@@ -51,26 +57,51 @@ void parse(vector<string> *wordbank, int setting){
         else {
             for ( int i = 0; i < holder.length(); i++){
                 if ( holder[i] != '\'' && holder[i] != '.' && holder[i] != ')' && holder[i] != '(' && holder[i] != '!' && holder[i] != '?' && holder[i] != '$' 
-                  && holder[i] != ',' && holder[i] != '-' && holder[i] != ';' && holder[i] != ':' && holder[i] != '&' && holder[i] != '\"' && holder[i] != '*')
+                     && holder[i] != ',' && holder[i] != '-' && holder[i] != ';' && holder[i] != ':' && holder[i] != '&' && holder[i] != '\"' && holder[i] != '*')
+                {
                     word = word + holder[i];
-                    transform(word.begin(), word.end(), word.begin(), ::tolower);     
+                }    
+
+                transform(word.begin(), word.end(), word.begin(), ::tolower);     
             }
             
-            if (word != "" && (uniqueWords.find(word) == uniqueWords.end()) ) 
+            if (word != "" && (uniqueWords.find(word) == uniqueWords.end()) ) {
                 wordbank->push_back(word);
-
-            if( (uniqueWords.find(word) == uniqueWords.end()) && word != "" ) {
                 uniqueWords[word] = word; 
-            }
-            
+            }   
             word="";
-
         }
     }
     sort(wordbank->begin(), wordbank->end());
     infile1.close();
 }
 
-void present ( vector<int> *features, vector<string> *wordbank, int setting){
+// 0 = training
+// 1 = testing
+void featurize ( vector<vector <int>> *features, vector<string> wordbank, int setting){
+    ifstream infile1;
+    string line, word;
+
+    int wordBanklength = wordbank.size();
     
+    if ( setting == 0 )
+        infile1.open("trainingSet.txt");
+    else 
+        infile1.open("testSet.txt");
+    
+    getline(infile1, line);
+
+        istringstream iss(line);
+        while ( iss >> word ){
+            for ( int i = 0; i < word.length(); i++){
+                if ( word[i] != '\'' && word[i] != '.' && word[i] != ')' && word[i] != '(' && word[i] != '!' && word[i] != '?' && word[i] != '$' 
+                     && word[i] != ',' && word[i] != '-' && word[i] != ';' && word[i] != ':' && word[i] != '&' && word[i] != '\"' && word[i] != '*')
+                {
+                    word = word + word[i];
+                }
+                transform(word.begin(), word.end(), word.begin(), ::tolower);    
+                for 
+            }
+        }
+        // cout << line << endl;
 }
